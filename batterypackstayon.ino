6fb91d74edf4a2ccc7a255da102b98115331adf5
NaubaharAgha <naubahar@sragha.com>
2019-03-28 15:07:59 +0100

    Dropped time to wait
    - Anker battery pack turned off between 4 to 5 seconds so time to wait decreased from 5 secs to 3 seconds.
    - Added circuit diagram

--- batterypackstayon.ino
+++ batterypackstayon.ino
@@ -1,8 +1,5 @@
-// Code and circuit diagram found here: https://www.held-im-ruhestand.de/hardware/powerbank-autooff.html
-// 
-
 const int trigPin = 13; // use pin 13 to show an LED pulse each time a pulse is sent out
-const int msToWait = 5000; //ms to wait for between each pulse
+const int msToWait = 3000; //ms to wait for between each pulse
 const int switchDelay = 500; //ms to wait between toggling high and low
 
 void setup() {
@@ -11,7 +8,7 @@ void setup() {
 
 void loop() {
   digitalWrite(trigPin, HIGH);
-  delay(500);
+  delay(switchDelay);
   digitalWrite(trigPin, LOW);
   delay(msToWait);
 }
